391a6de3de8a9358ba2339a43a9c7dad2d06cfae
sohei.harada <sohei.harada@avantcorp.com>
2025-06-12 10:41:52 +0900

    最初のコミット

---
+++ src/Operator.java
@@ -0,0 +1,15 @@
+public class Operator {
+public static void main(String[]args){
+    int a = 10 ;
+    int b = 3 ;
+    System.out.println("加算:"+ (a+ b));
+    System.out.println("割り算: "+(a / b));
+    System.out.println("余り: "+(a % b));
+
+    double power = Math.pow(a,b);
+    System.out.println("累乗: "+power);
+
+}
+       }
+       
+
